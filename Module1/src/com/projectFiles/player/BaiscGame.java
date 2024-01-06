2eb3373afc0950c765dbd7fa75eac5138a209b50
fanzehao <zehao.fan@mail.utoronto.ca>
2024-01-06 11:52:26 -0500

    Finished the project

--- Module1/src/com/projectFiles/player/BaiscGame.java
+++ Module1/src/com/projectFiles/player/BaiscGame.java
@@ -2,6 +2,8 @@ package com.projectFiles.player;
 
 import java.util.Random;
 import java.util.StringJoiner;
+import com.projectFiles.view.GameView;
+import javafx.application.Platform;
 
 public class BaiscGame {
 
@@ -18,28 +20,54 @@ public class BaiscGame {
 //    All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
 
-    static int[][] grid = new int[9][9];
+    public static int[][] grid = new int[20][20];
     public static Random random = new Random();
+    public static Boolean start = false;
+
+    /*
+    Clear all of the alive cells
+     */
+    public static void clearGrid() {
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                grid[i][j] = 0;
+            }
+        }
+    }
+
     /*
     j = col
     i = row
+    1 = alive
+    0 = dead
      */
+    public static void generateGrid() {
 
-    public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+
+        for (int i = 4; i < 16; i++) {
+            for (int j = 4; j < 17; j++) {
                 grid[i][j] = random.nextInt(2);
             }
         }
-        System.out.println("init---------------init");
-        printGrid();
-        int changed = 1;
-        // 1 == survive; 0 = died
-        while (changed != 0) {
-            // infinite loop, never terminate
-            changed = 0;
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
+
+
+    }
+
+
+    /*
+    Start the game
+     */
+    public static void startGame() {
+        System.out.println(start);
+        while (true) {
+            System.out.println(start);
+            if (!start) {
+                return;
+            }
+            printGrid();
+
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 20; j++) {
                     int cell1 = grid[i][j];
                     // case1: the position is 1
                     final int num_nei = checkNeigh(i, j);
@@ -47,12 +75,11 @@ public class BaiscGame {
                         if (num_nei == 0 || num_nei == 1) {
                             //Each cell with one or no neighbors dies, as if by solitude.
                             grid[i][j] = 0;
-                            changed = 1;
+
                         }
                         //Each cell with four or more neighbors dies,
                         else if (num_nei >= 4) {
                             grid[i][j] = 0;
-                            changed = 1;
                         }
                         //Each cell with two or three neighbors survives.
                         else {
@@ -65,7 +92,6 @@ public class BaiscGame {
                     else {
                         if (num_nei == 3) {
                             grid[i][j] = 1;
-                            changed = 1;
                         }
 
 
@@ -74,15 +100,79 @@ public class BaiscGame {
                 }
             }
 
-        printGrid();
+            Platform.runLater(() -> {
+                GameView.createOrUpdateGridPane();
+            });
+
+            try {
+                Thread.sleep(850);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+
+    }
+
+
+
+    /*
+    This represent only one role of change of the grid
+     */
+    public static void oneChange(){
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                int cell1 = grid[i][j];
+                // case1: the position is 1
+                final int num_nei = checkNeigh(i, j);
+                if (cell1 == 1) {
+                    if (num_nei == 0 || num_nei == 1) {
+                        //Each cell with one or no neighbors dies, as if by solitude.
+                        grid[i][j] = 0;
+
+                    }
+                    //Each cell with four or more neighbors dies,
+                    else if (num_nei >= 4) {
+                        grid[i][j] = 0;
+                    }
+                    //Each cell with two or three neighbors survives.
+                    else {
+                        //grid[i][j] = 1; since grid[i][j] = 1 is 1 already, so don't need to do it again
+                        continue;
+                    }
+
+                }
+                //case2: the position is 0
+                else {
+                    if (num_nei == 3) {
+                        grid[i][j] = 1;
+                    }
+
+
+                }
+
+            }
+        }
+
+        printGrid();
+
+
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        generateGrid();
+        System.out.println("init---------------init");
+        startGame();
+        printGrid();
+
+
     }
 
 
     public static void printGrid() {
         StringJoiner joiner = new StringJoiner("");
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
                 joiner.add(grid[i][j] + ", ");
             }
             joiner.add("\n");
@@ -104,16 +194,16 @@ public class BaiscGame {
     public static int checkNeigh(int i, int j) {
         int counter = 0;
 
-        int [] i_lst = {i-1, i, i+1};
-        int [] j_lst = {j-1, j, j+1};
+        int[] i_lst = {i - 1, i, i + 1};
+        int[] j_lst = {j - 1, j, j + 1};
 
         for (int i1 : i_lst) {
-            for (int j1 : j_lst){
+            for (int j1 : j_lst) {
                 // only count the cell's neighbors, don't count the cell itself
                 // check if the neighbors in in range(0,8)
-                if (0<=i1 && i1 <=8 && 0<=j1 && j1 <=8) {
-                    if (!(i==i1 && j == j1)) {
-                        if (grid[i1][j1]==1) {
+                if (0 <= i1 && i1 <= 19 && 0 <= j1 && j1 <= 19) {
+                    if (!(i == i1 && j == j1)) {
+                        if (grid[i1][j1] == 1) {
                             counter++;
                         }
                     }
@@ -122,6 +212,10 @@ public class BaiscGame {
         }
         return counter;
     }
+
+
+
+
 
 
 
--- Module1/src/com/projectFiles/view/GameView.java
+++ Module1/src/com/projectFiles/view/GameView.java
@@ -9,49 +9,74 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class GameView extends Application {
 
     BaiscGame model;
     Stage stage;
 
-    GridPane gridPane = new GridPane(90, 90);
+    static GridPane gridPane = new GridPane(90, 90);
     VBox vBox = new VBox(gridPane);
-    Button[][] cellButtons = new Button[9][9];
-    Boolean green = true;
+    static Boolean pink = false;
     GridPane large_grid = new GridPane();
-    VBox buttonBox = new VBox();
-    Button timeButton, musicButton, start_or_pauseButton, instructionButton;
+    Button timeButton, musicButton, start_or_pauseButton, instructionButton, clearButton, randomGenerate;
 
     Boolean timeToggle = false;
     Boolean instructionToggle = false;
+    Boolean started = false; // If the game started
 
 
     VBox boxTime = new VBox();
     VBox boxInstruction = new VBox();
 
-    public void createGrid() {
+    /*
+    pink = 1 = alive
+    blue = 0 = dead
+     */
+    public static void createOrUpdateGridPane() {
+
         gridPane.setHgap(2);
         gridPane.setVgap(2);
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        gridPane.getChildren().removeAll();
+        int[][] grid = BaiscGame.grid;
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
                 Button button = new Button();
                 button.setMinHeight(35);
                 button.setMinWidth(35);
                 button.setPrefSize(10, 10);
-                button.setStyle("-fx-background-color: green;");
+
+
+                if (grid[i][j]==1) {
+                    button.setStyle("-fx-background-color: pink;");
+                    pink = true;
+                } else {
+                    button.setStyle("-fx-background-color: blue;");
+                    pink = false;
+                }
+
+                final int row = i; // Make 'i' effectively final
+                final int col = j; // Make 'j' effectively final
+
                 button.setOnAction(s -> {
-                    if (green) {
+                    Background background = button.getBackground();
+                    final Color fillColor = (Color) background.getFills().get(0).getFill();
+                    if (fillColor.equals(Color.PINK)) {
                         button.setStyle("-fx-background-color: blue;");
-                        green = false;
+                        grid[row][col] = 0;
+                        pink = false;
                     } else {
-                        button.setStyle("-fx-background-color: green;");
-                        green = true;
+                        button.setStyle("-fx-background-color: pink;");
+                        grid[row][col] = 1;
+                        pink = true;
                     }
                 });
 
@@ -64,8 +89,13 @@ public class GameView extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        boxTime.setStyle("-fx-background-color: black");
+        boxInstruction.setStyle("-fx-background-color: black");
+
+
+
         stage.setTitle("The Game view");
-        createGrid();
+        createOrUpdateGridPane();
         large_grid.setPadding(new Insets(60));
         large_grid.setBackground(new Background(new BackgroundFill(
                 Color.valueOf("#FFFFFF"),
@@ -92,7 +122,23 @@ public class GameView extends Application {
         customizeButton(instructionButton, 150, 50);
         addInstructionEvent();
 
-        vBox.getChildren().addAll(musicButton, timeButton, instructionButton);
+        start_or_pauseButton = new Button("Start/pause");
+        start_or_pauseButton.setId("Start/pause");
+        customizeButton(start_or_pauseButton, 150, 50);
+        addStartPauseEvent();
+
+        clearButton = new Button("Clear");
+        clearButton.setId("clear");
+        customizeButton(clearButton, 100, 50);
+        addClearEvent();
+
+        randomGenerate = new Button("randomGenerate");
+        randomGenerate.setId("randomGenerate");
+        customizeButton(randomGenerate, 150, 50);
+        addRandomEvent();
+
+
+        vBox.getChildren().addAll(musicButton, timeButton, instructionButton, start_or_pauseButton, clearButton, randomGenerate);
         vBox.setSpacing(5);
 
 
@@ -100,7 +146,26 @@ public class GameView extends Application {
         large_grid.add(vBox, 1, 0, 1, 1);
         large_grid.setHgap(10);
         large_grid.setVgap(10);
-        Scene scene = new Scene(large_grid, 700, 500);
+
+        File file = new File("Module1/src/com/projectFiles/GameFiles/images/background2.jpg");
+        System.out.println(file.exists());
+        Image backgroundImage = new Image(file.toURI()+"", 1300, 1000,true,true);
+
+        // Create a background image
+        BackgroundImage background = new BackgroundImage(
+                backgroundImage,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT
+        );
+
+        // Set the background image to the root pane
+
+        large_grid.setBackground(new Background(background));
+
+        // Create the scene
+        Scene scene = new Scene(large_grid, 1200, 900);
         stage.setScene(scene);
         stage.show();
     }
@@ -130,20 +195,27 @@ public class GameView extends Application {
      * Time button.
      */
     public void addTimeEvent() {
-        large_grid.getChildren().remove(0,0);
+
         timeButton.setOnAction(e -> {
             // if timeTaggle = false; post the time and set it to true
+            large_grid.getChildren().remove(gridPane);
+            large_grid.getChildren().remove(boxInstruction);
+            large_grid.getChildren().remove(boxTime);
+            boxTime = new VBox();
 
             if (this.timeToggle) {
                 // remove the time and turn on the grid
 
                 this.large_grid.getChildren().remove(boxTime);
+                this.large_grid.getChildren().remove(boxInstruction);
 
                 this.large_grid.add(gridPane, 0, 0); // add the boxTime to the cell (0,0)
                 this.timeToggle = false;
             } else {
                 // remove the grid and turn on the time
-                this.large_grid.getChildren().remove(gridPane);
+                large_grid.getChildren().remove(gridPane);
+                large_grid.getChildren().remove(boxInstruction);
+
                 Clock instanceClock = Clock.getInstanceClock();
                 String timeStr = instanceClock.CurrentTime();
                 Text text = new Text(timeStr);
@@ -152,10 +224,12 @@ public class GameView extends Application {
                 text.setStyle("-fx-fill: #FFFFFF"); // set the color of the text to white
                 text.setWrappingWidth(200);// set the length of each line
                 // of word
+                boxTime.getChildren().removeAll();
                 boxTime.getChildren().add(text);
                 boxTime.setPadding(new Insets(10));
                 boxTime.setAlignment(Pos.TOP_LEFT);
                 boxTime.setStyle("-fx-background-color: " + "#000000" + ";");
+
 
                 this.large_grid.add(boxTime, 0, 0);
                 this.timeToggle = true;
@@ -167,36 +241,156 @@ public class GameView extends Application {
     }
 
 
+    /*
+     Show the instructions of how to play the game
+     */
     public void addInstructionEvent() {
-        instructionButton.setOnAction(s->{
-                String instruction =
-                        "Each cell with one or no neighbors dies, as if by solitude.\n" +
-                        "\n" +
-                        "\n" +
-                        "Each cell with four or more neighbors dies, as if by overpopulation.\n" +
-                        "\n" +
-                        "\n" +
-                        "Each cell with two or three neighbors survives.\n" +
-                        "\n" +
-                        "\n" +
-                        "For a space that is empty or unpopulated:\n" +
-                        "Each cell with three neighbors becomes populated.";
 
-                // If it is false
+        instructionButton.setOnAction(s -> {
+            large_grid.getChildren().remove(gridPane);
+            large_grid.getChildren().remove(boxInstruction);
+            large_grid.getChildren().remove(boxTime);
+
+
+            String instruction =
+                    "Each cell with one or no neighbors dies, as if by solitude.\n" +
+                            "\n" +
+                            "\n" +
+                            "Each cell with four or more neighbors dies, as if by overpopulation.\n" +
+                            "\n" +
+                            "\n" +
+                            "Each cell with two or three neighbors survives.\n" +
+                            "\n" +
+                            "\n" +
+                            "For a space that is empty or unpopulated:\n" +
+                            "Each cell with three neighbors becomes populated.";
+
+            // If it is false
             if (!instructionToggle) {
                 Text text = new Text(instruction);
+                text.setFont(new Font(15));
+                text.setStyle("-fx-fill: #000000"); // set the color of the text to white
+                boxInstruction  = new VBox();
                 boxInstruction.getChildren().add(text);
                 large_grid.getChildren().remove(gridPane);
-                large_grid.add(boxInstruction, 0,0);
+                large_grid.getChildren().remove(boxTime);
+
+                large_grid.add(boxInstruction, 0, 0);
                 this.instructionToggle = true;
             } else {
-                large_grid.getChildren().remove(boxInstruction);
-                large_grid.add(gridPane,0,0);
-                this.instructionToggle= false;
+                this.large_grid.getChildren().remove(boxTime);
+                this.large_grid.getChildren().remove(boxInstruction);
+                large_grid.add(gridPane, 0, 0);
+                this.instructionToggle = false;
             }
         });
 
     }
+
+
+    /*
+    Clear the grid
+     */
+    public void addClearEvent() {
+        clearButton.setOnAction(s -> {
+            BaiscGame.clearGrid();
+            final int size = gridPane.getChildren().size();
+            for (int i = 0; i < size; i++) {
+                final Button button1 = (Button) gridPane.getChildren().get(i);
+                button1.setStyle("-fx-background-color: blue;");
+            }
+
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 20; j++) {
+                    BaiscGame.grid[i][j] = 0;
+                }
+            }
+        });
+
+
+    }
+
+
+    /*
+        Randomly generate a grid
+     */
+
+    public void addRandomEvent() {
+        randomGenerate.setOnAction(s -> {
+            BaiscGame.generateGrid();
+            final int[][] grid = BaiscGame.grid;
+
+            gridPane.setHgap(2);
+            gridPane.setVgap(2);
+            gridPane.getChildren().removeAll();
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 20; j++) {
+                    Button button = new Button();
+                    button.setMinHeight(35);
+                    button.setMinWidth(35);
+                    button.setPrefSize(10, 10);
+                    final int row = i;
+                    final int col = j;
+                    button.setOnAction(s1 -> {
+                        Background background = button.getBackground();
+                        final Color fillColor = (Color) background.getFills().get(0).getFill();
+                        if (fillColor.equals(Color.PINK)) {
+                            button.setStyle("-fx-background-color: blue;");
+                            grid[row][col] = 0;
+                            pink = false;
+                        } else {
+                            button.setStyle("-fx-background-color: pink;");
+                            grid[row][col] = 1;
+                            pink = true;
+                        }
+                    });
+                    if (grid[i][j] == 1) {
+                        button.setStyle("-fx-background-color: pink;");
+
+                    } else {
+                        button.setStyle("-fx-background-color: blue;");
+
+                    }
+                    gridPane.add(button, i, j);
+                }
+            }
+        });
+
+
+    }
+
+
+
+    /*
+        Add start and pause event
+     */
+
+    public void addStartPauseEvent(){
+        start_or_pauseButton.setOnAction(s->{
+            if (!started) {
+                System.out.println("going to start");
+                started = true;
+                new Thread(() -> {
+                    BaiscGame.start = true;
+                    BaiscGame.startGame();
+                }).start();
+
+
+            }
+            else {
+                System.out.println("going to the start pause event else");
+                started = false;
+                BaiscGame.start = false;
+                System.out.println("Base game start = flase");
+
+            }
+        });
+
+    }
+
+
+
+
 
 
 }
